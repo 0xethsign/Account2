3083ab575f63fc2715d37129d72e1134f2a3b25b
Joseph Delong <joseph@delong.me>
2022-12-03 23:51:32 +0530

    separate execution and economic abstraction

--- src/Account2.sol
+++ src/Account2.sol
@@ -4,27 +4,51 @@ pragma solidity ^0.8.15;
 import "solmate/auth/Owned.sol";
 import "solmate/auth/Auth.sol";
 
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+}
 contract Account2 is Owned {
     constructor() Owned(msg.sender) {}
 
     function executeAsRelay(
         address toCall,
         bytes calldata params,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+
+        // validate signature
+        bytes32 hash = keccak256(abi.encodePacked(toCall, params));
+        address signer = ecrecover(hash, v, r, s);
+        require(signer == owner && signer != address(0), "invalid signature");
+
+        (bool success, bytes memory data) = toCall.call(params);
+    }
+
+    function executeWithEconomicAbstraction(
+        address toCall,
+        bytes calldata params,
         uint256 gasTokenRatio,  // preferably >x, x = gas price in token
-        address gasToken) external {
-            // (bool success, bytes memory data) = toCall.call(bytes.concat(select, params));
-            uint256 gasUsed = gasleft();
-            (bool success, bytes memory data) = toCall.call(params);
+        address gasToken
+    ) internal {
 
-            gasUsed -= gasleft() + 21000;
+        uint256 gasUsed = gasleft();
+        (bool success, bytes memory data) = toCall.call(params);
+        gasUsed -= gasleft() + 21000;
 
-            uint256 amountToTransfer = (gasUsed * gasTokenRatio);
+        uint256 amount = (gasUsed * gasTokenRatio);
+        uint256 balance = IERC20(gasToken).balanceOf(address(this));
+        amount = amount > balance ? balance : amount;
+        require(IERC20(gasToken).transfer(tx.origin, amount), 'gasToken balance too low');
     }
 
     function executeAsOwner(
         address toCall,
         bytes calldata params
     ) external onlyOwner {
+        
         (bool success, bytes memory data) = toCall.call(params);
     }
 }
