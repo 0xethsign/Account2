3cae6642b25511750d1c8b2fde22d8bd63e209f2
Joseph Delong <joseph@delong.me>
2022-12-03 23:06:47 +0530

    removed gasLimit from the design

--- src/Account2.sol
+++ src/Account2.sol
@@ -10,7 +10,6 @@ contract Account2 is Owned {
     function executeAsRelay(
         address toCall,
         bytes calldata params,
-        uint256 gasLimit,
         uint256 gasTokenRatio,  // preferably >x, x = gas price in token
         address gasToken) external {
             // (bool success, bytes memory data) = toCall.call(bytes.concat(select, params));
@@ -18,7 +17,6 @@ contract Account2 is Owned {
             (bool success, bytes memory data) = toCall.call(params);
 
             gasUsed -= gasleft() + 21000;
-            require(gasUsed <= gasLimit, 'gas limit exceeded');
 
             uint256 amountToTransfer = (gasUsed * gasTokenRatio);
     }
